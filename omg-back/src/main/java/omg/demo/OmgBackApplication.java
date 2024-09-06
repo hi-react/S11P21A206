e938e05b110b91d4ae9c73d058505e29a2cd2ebd
nowalex322 <nowalex322@naver.com>
2024-09-06 10:53:42 +0900

    Init: 백엔드  프로젝트 파일 초기세팅 및 생성

---
+++ omg-back/src/main/java/omg/demo/OmgBackApplication.java
@@ -0,0 +1,13 @@
+package omg.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmgBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmgBackApplication.class, args);
+	}
+
+}
---
+++ omg-back/src/test/java/omg/demo/OmgBackApplicationTests.java
@@ -0,0 +1,13 @@
+package omg.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmgBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
