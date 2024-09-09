82135b193666ace47a5daa131f8594b3978b9c12
nowalex322 <nowalex322@naver.com>
2024-09-09 16:29:37 +0900

    feat: Add redis files and Update WebSocketConfig

--- omg-back/src/main/java/com/ssafy/omg/config/WebSocketConfig.java
+++ omg-back/src/main/java/com/ssafy/omg/config/WebSocketConfig.java
@@ -1,4 +1,14 @@
 package com.ssafy.omg.config;
 
-public class WebSocketConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
 }
