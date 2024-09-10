56f24e859065ffa6aee1af30dcbc3fbfdaf9b087
HannaKang <hanna0175@gmail.com>
2024-09-10 17:50:51 +0900

    [#7] Chore: 페이지별 파일 생성 및 초기 성능개선 세팅
    - 코드 스플리팅 적용
    - 레이지 로드 적용
    - 로그인, 대기방, 인게임 페이지 생성

--- omg-front/src/main.tsx
+++ omg-front/src/main.tsx
@@ -1,5 +1,15 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { Suspense, lazy } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
-createRoot(document.getElementById("root")!).render(<App />);
+import './index.css';
+
+const App = lazy(() => import('./App'));
+
+createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+    <Suspense fallback={<div>앱 불러오는 중...</div>}>
+      <App />
+    </Suspense>
+  </BrowserRouter>,
+);
