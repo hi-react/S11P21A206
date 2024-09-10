ea4062963715ee39f9df6ba501a94dd8bf95f226
Now <khj990322@gmail.com>
2024-09-11 05:51:15 +0900

    [#4] Feat: Add WebSocket Event Listener and test html file

---
+++ omg-back/src/main/java/com/ssafy/omg/config/WebSocketEventListener.java
@@ -0,0 +1,50 @@
+package com.ssafy.omg.config;
+
+import com.ssafy.omg.config.baseresponse.BaseException;
+import com.ssafy.omg.domain.room.dto.CommonRoomRequest;
+import com.ssafy.omg.domain.room.dto.CommonRoomResponse;
+import com.ssafy.omg.domain.room.service.RoomService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class WebSocketEventListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(WebSocketEventListener.class);
+    private final SimpMessageSendingOperations messagingTemplate;
+    private final RoomService roomService;
+
+    @EventListener
+    public void handleWebSocketConnectListener(SessionConnectedEvent event) {
+        logger.info("새 웹소켓 연결");
+    }
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String userNickname = (String) headerAccessor.getSessionAttributes().get("userNickname");
+        String roomId = (String) headerAccessor.getSessionAttributes().get("roomId");
+
+        if(userNickname != null && roomId != null) {
+            logger.info("연결 해제 : " + userNickname);
+
+            try {
+                CommonRoomResponse response = roomService.leaveRoom(new CommonRoomRequest(roomId, userNickname, "LEAVE_GAME"));
+                messagingTemplate.convertAndSend("/topic/game/" + roomId, response);
+            } catch (BaseException e) {
+                logger.error("Error processing user disconnect: ", e);
+            }
+        }
+    }
+}
