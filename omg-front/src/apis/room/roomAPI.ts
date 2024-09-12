9d0d42224361e6bb78c777064ee575e5cb7106e3
HannaKang <hanna0175@gmail.com>
2024-09-12 18:41:23 +0900

    [#6] Feat: 로비에서 방입장 API연동

--- omg-front/src/apis/room/roomAPI.ts
+++ omg-front/src/apis/room/roomAPI.ts
@@ -25,3 +25,25 @@ export const createWaitingRooms = async (userNickname: string) => {
 };
 
 //대기방 입장
+export const enterWaitingRoom = async (roomId: string, sender: string) => {
+  try {
+    const response = await axiosInstance.post(`${END_POINT.ROOM}/enter`, {
+      roomId,
+      sender,
+      message: 'ENTER_SUCCESS',
+    });
+
+    const { message, roomId: returnedRoomId } = response.data.result;
+    if (message === 'ENTER_SUCCESS') {
+      console.log(`${returnedRoomId} 방입장 성공`);
+      return { success: true, roomId: returnedRoomId };
+    } else {
+      console.warn(`방입장 실패: ${message}`);
+      return { success: false, message };
+    }
+  } catch (error: unknown) {
+    const err = error as InstanceError;
+    console.error(err.response?.status, err.message);
+    throw error;
+  }
+};
