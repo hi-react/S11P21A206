dc960d54e084f34c5d06547ba9c5f0210a288201
Now <khj990322@gmail.com>
2024-09-13 14:43:15 +0900

    [#9] Refactor: refactor Dirs

--- omg-back/src/main/java/com/ssafy/omg/domain/room/service/RoomService.java
+++ omg-back/src/main/java/com/ssafy/omg/domain/room/service/RoomService.java
@@ -3,7 +3,7 @@ package com.ssafy.omg.domain.room.service;
 import com.ssafy.omg.config.baseresponse.BaseException;
 import com.ssafy.omg.domain.room.dto.CommonRoomRequest;
 import com.ssafy.omg.domain.room.dto.CommonRoomResponse;
-import com.ssafy.omg.domain.room.entity.RoomInfo;
+import com.ssafy.omg.domain.room.entity.Room;
 
 public interface RoomService {
 
@@ -32,8 +32,8 @@ public interface RoomService {
     CommonRoomResponse checkAllRenderedCompleted(CommonRoomRequest request) throws BaseException;
 
     // 방 정보 반환
-    RoomInfo getRoomInfo(String roomId) throws BaseException;
+    Room getRoomInfo(String roomId) throws BaseException;
 
     // 방 정보 업데이트
-    void updateRoom(RoomInfo room) throws BaseException;
+    void updateRoom(Room room) throws BaseException;
 }
