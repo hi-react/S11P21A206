88e4f99c807de5dbedc7218a52a03196e2330d7c
이가은 <rk243812@gmail.com>
2024-09-19 17:47:03 +0900

    [#11] Feat: 상환 구현
    - done: 상환 로직 구현
    - done: Game, Player setter 삭제
    - done: Player에 대출 관련 필드 수정

--- omg-back/src/main/java/com/ssafy/omg/domain/game/service/GameServiceImpl.java
+++ omg-back/src/main/java/com/ssafy/omg/domain/game/service/GameServiceImpl.java
@@ -17,13 +17,14 @@ import static com.ssafy.omg.config.baseresponse.BaseResponseStatus.*;
 @RequiredArgsConstructor
 public class GameServiceImpl implements GameService {
 
-	private final RedisTemplate<String, Arena> redisTemplate;
-	// Redis에서 대기방 식별을 위한 접두사 ROOM_PREFIX 설정
-	private static final String ROOM_PREFIX = "room";
-	private final int[][] LOAN_RANGE = new int[][]{{50, 100}, {150, 300}, {500, 1000}};
-	private final GameController gameController;
+    private final RedisTemplate<String, Arena> redisTemplate;
+    // Redis에서 대기방 식별을 위한 접두사 ROOM_PREFIX 설정
+    private static final String ROOM_PREFIX = "room";
+    // 대출 가능 범위
+    private final int[][] LOAN_RANGE = new int[][]{{50, 100}, {150, 300}, {500, 1000}};
+    private final GameController gameController;
 
-	// 초기화
+    // 초기화
 //	public GameInfo initializeGame(String gameId, List<String> players) {
 //		GameInfo gameInfo = new GameInfo();
 //
@@ -50,151 +51,171 @@ public class GameServiceImpl implements GameService {
 //		return gameInfo;
 //	}
 
-
-	// 대출
-
-	/**
-	 * 대출
-	 * 0. 거래소에서 사채업자 클릭해 대출 선택
-	 * 1. (preLoan) 대출 가능한 금액과 횟수 표시
-	 * 2. (preLoan) 대출 여부를 체크해서 대출 가능 여부 판단
-	 * 3. (takeLoan) 가능 대출 범위 내에서 원하는 금액 대출
-	 * 4. (takeLoan) 대출금을 자산에 반영
-	 * 5. 대출 종료
-	 * <p>
-	 * 대출횟수는 1회
-	 * <p>
-	 * 가능 대출 범위 :
-	 * 주가수준 0~2일때 : 50~100
-	 * 주가수준 3~5일때 : 150~300
-	 * 주가수준 6~9일때 : 500~1000
-	 *
-	 * @param
-	 * @throws BaseException
-	 */
-	public int preLoan(String roomId, String sender) throws BaseException {
-		String roomKey = ROOM_PREFIX + roomId;
-
-		// 입력값 오류
-		validateRequest(roomId, sender);
-
-		Arena arena = getArena(roomKey);
-		Player player = getPlayer(arena, sender);
-
-		// 이미 대출을 받은 적 있으면 0 리턴
-		if (player.getHasLoan() == 1) {
-			throw new BaseException(LOAN_ALREADY_TAKEN);
-		}
-
-		// 대출 가능 금액 계산
-		int stockPriceLevel = arena.getGame().getStockPriceLevel();
-		if (stockPriceLevel < 0 || stockPriceLevel > 9) {
-			throw new BaseException(INVALID_STOCK_LEVEL);
-		} else if (stockPriceLevel <= 2) {
-			return 0;
-		} else if (stockPriceLevel <= 5) {
-			return 1;
-		} else {
-			return 2;
-		}
-	}
-
-	/**
-	 * @param roomId
-	 * @param sender
-	 * @param amount
-	 * @throws BaseException
-	 */
-	public void takeLoan(String roomId, String sender, int amount) throws BaseException {
-		String roomKey = ROOM_PREFIX + roomId;
-
-		validateRequest(roomId, sender);
-		int range = preLoan(roomId, sender);
-
-		// 요청 금액이 대출 한도를 이내인지 검사
-		if (amount <= LOAN_RANGE[range][0] || LOAN_RANGE[range][1] <= amount) {
-			throw new BaseException(AMOUNT_OUT_OF_RANGE);
-		}
-
-		// 대출금을 자산에 반영
-		Arena arena = getArena(roomKey);
-		Player player = getPlayer(arena, sender);
-
-		player.setHasLoan(1);
-		player.setLoan(amount);
-		player.setInterestRate(arena.getGame().getInterestRate());
-		player.setCash(player.getCash() + amount);
-
-		savePlayer(roomKey, arena, player);
-	}
-
-	// 상환
+    // TODO
+    // preLoan을 api 따로 빼면 수정
+    // 플레이어 행위 상태(state)는 언제 어디서 업데이트 해줘야 할지
 
 
-	// 주식 매수
+    // 대출
 
-	// 주식 매도
+    /**
+     * [preLoan] 대출 가능 여부 판단 후, 대출 금액 범위 리턴
+     *
+     * @param
+     * @throws BaseException
+     */
+    public int preLoan(String roomId, String sender) throws BaseException {
+        String roomKey = ROOM_PREFIX + roomId;
 
-	// 금괴 매입
+        // 입력값 오류
+        validateRequest(roomId, sender);
 
-	/**
-	 * 요청의 입력유효성 검사
-	 *
-	 * @param sender 요청을 보낸 사용자의 닉네임
-	 * @throws BaseException roomId나 sender가 null이거나 비어있을 경우 발생
-	 */
-	private void validateRequest(String roomId, String sender) throws BaseException {
-		if (roomId == null || roomId.isEmpty() || sender == null || sender.isEmpty()) {
-			throw new BaseException(REQUEST_ERROR);
-		}
-	}
+        Arena arena = getArena(roomKey);
+        Player player = getPlayer(arena, sender);
 
-	/**
-	 * Redis에서 arena를 가져오기
-	 *
-	 * @param roomKey Redis에서 사용하는 방의 키
-	 * @return 방의 arena
-	 * @throws BaseException 방을 찾을 수 없을 경우 발생
-	 */
-	private Arena getArena(String roomKey) throws BaseException {
-		Arena arena = redisTemplate.opsForValue().get(roomKey);
-		if (arena == null || arena.getGame() == null) {
-			throw new BaseException(GAME_NOT_FOUND);
-		}
-		return arena;
-	}
+        // 이미 대출을 받은 적 있으면 0 리턴
+        if (player.getHasLoan() == 1) {
+            throw new BaseException(LOAN_ALREADY_TAKEN);
+        }
+
+        // 대출 가능 금액 계산
+        int stockPriceLevel = arena.getGame().getStockPriceLevel();
+        if (stockPriceLevel < 0 || stockPriceLevel > 9) {
+            throw new BaseException(INVALID_STOCK_LEVEL);
+        } else if (stockPriceLevel <= 2) {
+            return 0;
+        } else if (stockPriceLevel <= 5) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+
+    /**
+     * [takeLoan] 요청금액 대출 후, 플레이어 자산 업데이트
+     *
+     * @param roomId
+     * @param sender
+     * @param amount
+     * @throws BaseException
+     */
+    public void takeLoan(String roomId, String sender, int amount) throws BaseException {
+        String roomKey = ROOM_PREFIX + roomId;
+
+        validateRequest(roomId, sender);
+        int range = preLoan(roomId, sender);
+
+        // 요청 금액이 대출 한도를 이내인지 검사
+        if (amount <= LOAN_RANGE[range][0] || LOAN_RANGE[range][1] <= amount) {
+            throw new BaseException(AMOUNT_OUT_OF_RANGE);
+        }
+
+        // 대출금을 자산에 반영
+        Arena arena = getArena(roomKey);
+        Player player = getPlayer(arena, sender);
+        int interest = amount * (arena.getGame().getInterestRate() / 100);
+        player = player.toBuilder()
+                .hasLoan(1)
+                .loan(amount)
+                .interest(interest)
+                .debt(amount + interest)
+                .cash(player.getCash() + amount).build();
+
+        savePlayer(roomKey, arena, player);
+    }
+
+    // 상환
+    public void repayLoan(String roomId, String sender, int amount) throws BaseException {
+        String roomKey = ROOM_PREFIX + roomId;
+
+        validateRequest(roomId, sender);
+
+        Arena arena = getArena(roomKey);
+        Player player = getPlayer(arena, sender);
+
+        // 상환 금액이 유효한 값인지 판단(음수, 갚아야 할 금액보다 큰 금액인 경우, 자신이 보유한 현금보다 큰 값인 경우)
+        if (amount < 0 || player.getDebt() < amount || player.getCash() < amount) {
+            throw new BaseException(INVALID_REPAY_AMOUNT);
+        }
+
+        // 상환 후 자산에 반영(갚아야 할 금액 차감, 현금 차감)
+        player = player.toBuilder()
+                .debt(player.getDebt() - amount)
+                .cash(player.getCash() - amount)
+                .build();
+
+        savePlayer(roomKey, arena, player);
+    }
 
 
-	/**
-	 * @param arena
-	 * @param sender
-	 * @return
-	 * @throws BaseException
-	 */
-	private Player getPlayer(Arena arena, String sender) throws BaseException {
-		List<Player> players = arena.getGame().getPlayers();
-		Optional<Player> currPlayer = players.stream()
-				.filter(player -> player.getNickname().equals(sender))
-				.findFirst();
+    // 주식 매수
 
-		if (currPlayer.isEmpty()) {
-			throw new BaseException(PLAYER_NOT_FOUND);
-		}
-		return currPlayer.get();
-	}
 
-	/**
-	 * player의 행위로 인해 변경된 값을 redis에 적용
-	 *
-	 * @param arena
-	 * @param currPlayer
-	 * @return
-	 */
-	private void savePlayer(String roomKey, Arena arena, Player currPlayer) {
-		arena.getGame().getPlayers().replaceAll(player ->
-				player.getNickname().equals(currPlayer.getNickname()) ? currPlayer : player
-		);
+    // 주식 매도
 
-		redisTemplate.opsForValue().set(roomKey, arena);
-	}
+
+    // 금괴 매입
+
+
+    /**
+     * 요청의 입력유효성 검사
+     *
+     * @param sender 요청을 보낸 사용자의 닉네임
+     * @throws BaseException roomId나 sender가 null이거나 비어있을 경우 발생
+     */
+    private void validateRequest(String roomId, String sender) throws BaseException {
+        if (roomId == null || roomId.isEmpty() || sender == null || sender.isEmpty()) {
+            throw new BaseException(REQUEST_ERROR);
+        }
+    }
+
+    /**
+     * Redis에서 arena를 가져오기
+     *
+     * @param roomKey Redis에서 사용하는 방의 키
+     * @return 방의 arena
+     * @throws BaseException 방을 찾을 수 없을 경우 발생
+     */
+    private Arena getArena(String roomKey) throws BaseException {
+        Arena arena = redisTemplate.opsForValue().get(roomKey);
+        if (arena == null || arena.getGame() == null) {
+            throw new BaseException(GAME_NOT_FOUND);
+        }
+        return arena;
+    }
+
+
+    /**
+     * arena에서 sender에 해당하는 player 가져오기
+     *
+     * @param arena
+     * @param sender
+     * @return arena game의 players 중 sender에 해당하는 player
+     * @throws BaseException
+     */
+    private Player getPlayer(Arena arena, String sender) throws BaseException {
+        List<Player> players = arena.getGame().getPlayers();
+        Optional<Player> currPlayer = players.stream()
+                .filter(player -> player.getNickname().equals(sender))
+                .findFirst();
+
+        if (currPlayer.isEmpty()) {
+            throw new BaseException(PLAYER_NOT_FOUND);
+        }
+        return currPlayer.get();
+    }
+
+    /**
+     * player의 행위로 인해 변경된 값을 redis에 업데이트
+     *
+     * @param arena
+     * @param currPlayer
+     */
+    private void savePlayer(String roomKey, Arena arena, Player currPlayer) {
+        arena.getGame().getPlayers().replaceAll(player ->
+                player.getNickname().equals(currPlayer.getNickname()) ? currPlayer : player
+        );
+
+        redisTemplate.opsForValue().set(roomKey, arena);
+    }
 }
--- omg-back/src/main/java/com/ssafy/omg/domain/player/entity/Player.java
+++ omg-back/src/main/java/com/ssafy/omg/domain/player/entity/Player.java
@@ -1,7 +1,7 @@
 package com.ssafy.omg.domain.player.entity;
 
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 /**
  * 플레이어 정보
@@ -9,23 +9,25 @@ import lombok.Setter;
  * - 플레이어 행위(무슨 행동을 하는지), 플레이어 행위 종료 상태, 플레이어 접속 상태, 한 행위 시간
  */
 @Getter
-@Setter
+@Builder(toBuilder = true)
 public class Player {
-	private String nickname;
+    private String nickname;
 
-	private double[] position;            // 플레이어 좌표(x, y, z)
-	private double[] direction;         // 플레이어 방향
+    private double[] position;            // 플레이어 좌표(x, y, z)
+    private double[] direction;         // 플레이어 방향
 
-	private int hasLoan;            // 대출 유무
-	private int loan;                // 대출금
-	private int interestRate;        // 이자율
-	private int cash;                // 현금
-	private int[] stock;            // 보유 주식 개수
-	private int gold;                // 보유 금괴 개수
+    private int hasLoan;            // 대출 유무
+    private int loan;                // 대출원금
+    private int interest;        // 이자
+    private int debt;                // 갚아야 할 금액
 
-	private PlayerAction action;    // 플레이어 행위 (주식 매수, 주식 매도, 금괴 매입, 대출, 상환)
-	private PlayerStatus state;        // 플레이어 행위 상태 (시작전, 진행중, 완료)
-	private int time;                // 플레이어의 한 행위 남은 시간
-	private int isConnected;        // 플레이어 접속 상태 (0: 끊김, 1: 연결됨)
+    private int cash;                // 현금
+    private int[] stock;            // 보유 주식 개수
+    private int gold;                // 보유 금괴 개수
+
+    private PlayerAction action;    // 플레이어 행위 (주식 매수, 주식 매도, 금괴 매입, 대출, 상환)
+    private PlayerStatus state;        // 플레이어 행위 상태 (시작전, 진행중, 완료)
+    private int time;                // 플레이어의 한 행위 남은 시간
+    private int isConnected;        // 플레이어 접속 상태 (0: 끊김, 1: 연결됨)
 
 }
