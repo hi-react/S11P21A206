9e4398f8c2169bc6c986d203cc09260c2616fbbd
이가은 <rk243812@gmail.com>
2024-09-19 12:50:08 +0900

    git commit -m "[#8] Feat: 대출 구현
    - done: 대출 로직 구현
    
    - need: 아직 대출 테스트 안함
    "

--- omg-back/src/main/java/com/ssafy/omg/domain/game/service/GameService.java
+++ omg-back/src/main/java/com/ssafy/omg/domain/game/service/GameService.java
@@ -1,69 +1,9 @@
 package com.ssafy.omg.domain.game.service;
 
-import com.ssafy.omg.domain.game.dto.CommonUserActionRequest;
-import com.ssafy.omg.domain.game.dto.GameInfo;
-import com.ssafy.omg.domain.player.dto.PlayerInfo;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Service;
+import com.ssafy.omg.config.baseresponse.BaseException;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+public interface GameService {
+	int preLoan(String roomId, String sender) throws BaseException;
 
-@Service
-@RequiredArgsConstructor
-public class GameService {
-
-    private final RedisTemplate<String, Object> redisTemplate;
-
-    public GameInfo initializeGame(String gameId, List<String> players) {
-        GameInfo gameInfo = new GameInfo();
-
-        gameInfo.setGameId(gameId);
-        gameInfo.setCurrentPosition(new int[]{0, 0, 0, 0});
-        gameInfo.setTurn(1);
-        gameInfo.setRound(1);
-        gameInfo.setGameStatus("BEFORE_GAME_PLAY");
-        gameInfo.setStartTime(java.time.LocalDateTime.now().toString());
-
-        Map<String, PlayerInfo> playerInfoMap = new HashMap<>();
-        for (int i = 0; i < players.size(); i++) {
-            PlayerInfo playerInfo = new PlayerInfo();
-            playerInfo.setNickname(players.get(i));
-            playerInfo.setGold(0);
-            playerInfo.setCash(200);
-            playerInfo.setToken(new int[]{0, 1, 2, 1, 1});
-            playerInfoMap.put(String.valueOf(i), playerInfo);
-        }
-        gameInfo.setPlayers(playerInfoMap);
-
-        redisTemplate.opsForValue().set("game:" + gameId, gameInfo);
-
-        return gameInfo;
-    }
-
-    /**
-     * 대출
-     * 0. 거래소에서 사채업자 클릭해 대출 선택
-     * 1. (preLoan) 대출 가능한 금액과 횟수 표시
-     * 2. (preLoan) 대출 여부를 체크해서 대출 가능 여부 판단
-     * 3. (takeLoan) 가능 대출 범위 내에서 원하는 금액 대출
-     * 4. (takeLoan) 대출금을 자산에 반영
-     * 5. 대출 종료
-     *
-     * 대출횟수는 1회
-     *
-     * 가능 대출 범위 :
-     *      주가수준 0~2일때 : 50~100
-     *      주가수준 3~5일때 : 150~300
-     *      주가수준 6~9일때 : 500~1000
-     *
-     * @param request
-     */
-    public void takeLoan(CommonUserActionRequest request) {
-
-    }
-
-
+	void takeLoan(String roomId, String sender, int amount) throws BaseException;
 }
