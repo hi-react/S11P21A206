32f0187a3a1840b9eadcddc886d5d87ee5a635e0
HannaKang <hanna0175@gmail.com>
2024-09-19 20:44:19 +0900

    [#10] Feat: WebSocket 연결 및 메시지 전송 기능 추가
    - `useEffect` 훅을 사용하여 WebSocket 연결 및 STOMP 클라이언트 설정 추가
    - 환경 변수에서 WebSocket URL을 불러와 연결
    - 방 구독 및 메시지 전송 기능 구현 중
    - `isConnected` 상태를 추가하여 연결 상태 관리

--- omg-front/src/pages/Waiting.tsx
+++ omg-front/src/pages/Waiting.tsx
@@ -1,14 +1,63 @@
+import { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+
+import { useRoomStore } from '@/stores/room';
+import { CompatClient, Frame, Stomp } from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
 
 export default function Waiting() {
   const navigate = useNavigate();
+  const { roomId, sender } = useRoomStore();
+  const client = useRef<CompatClient | null>(null);
+  const [isConnected, setIsConnected] = useState(false);
+  const socket_url = import.meta.env.VITE_APP_SOCKET_URL;
+
+  useEffect(() => {
+    const socket = new SockJS(socket_url);
+    const stompClient = Stomp.over(socket);
+
+    stompClient.connect({}, (frame: Frame) => {
+      setIsConnected(true);
+      console.log('Connected: ' + frame);
+
+      // 구독 설정
+      stompClient.subscribe(`/sub/${roomId}/room`, message => {
+        if (message.body) {
+          console.log('Received message:', JSON.parse(message.body));
+        }
+      });
+
+      // 메시지 전송
+      stompClient.send(
+        '/pub/room',
+        {},
+        JSON.stringify({
+          roomId,
+          sender,
+          message: 'LEAVE_ROOM',
+        }),
+      );
+    });
+
+    client.current = stompClient;
+
+    return () => {
+      if (client.current) {
+        client.current.disconnect(() => {
+          setIsConnected(false);
+          console.log('Disconnected');
+        });
+      }
+    };
+  }, [roomId, sender]);
 
   const handleClick = () => {
     navigate('/game');
   };
+
   return (
     <div>
-      <h2 className='text-lime-700'>waiting</h2>
+      <h2 className='text-lime-700'>Waiting</h2>
       <button onClick={handleClick}>START</button>
     </div>
   );
