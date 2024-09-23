d3c6b66cfc086d99b959f324d9cb17876c02c2f0
전정민 <junsk50@naver.com>
2024-09-23 17:56:17 +0900

    [#13] Feat: 플레이어 좌표 브로드캐스트 기능 구현

--- omg-back/src/main/java/com/ssafy/omg/domain/game/controller/GameController.java
+++ omg-back/src/main/java/com/ssafy/omg/domain/game/controller/GameController.java
@@ -3,6 +3,7 @@ package com.ssafy.omg.domain.game.controller;
 import com.ssafy.omg.config.baseresponse.BaseException;
 import com.ssafy.omg.config.baseresponse.BaseResponse;
 import com.ssafy.omg.domain.arena.entity.Arena;
+import com.ssafy.omg.domain.game.service.GameBroadcastService;
 import com.ssafy.omg.domain.game.service.GameService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
@@ -15,12 +16,22 @@ import java.util.List;
 @RequiredArgsConstructor
 public class GameController {
     private final GameService gameService;
+    private final GameBroadcastService gameBroadcastService;
     private final SimpMessageSendingOperations messagingTemplate;
 
     @PostMapping("/initialize")
     public BaseResponse<Arena> initializeGame(@RequestParam String roomId, @RequestBody List<String> players) throws BaseException {
         Arena arena = gameService.initializeGame(roomId, players);
+        gameBroadcastService.startBroadcast(roomId);
         return new BaseResponse<>(arena);
+    }
+
+    @PostMapping("/end-game")
+    public BaseResponse<Void> endGame(@RequestParam String roomId) {
+        gameBroadcastService.stopBroadcast(roomId);
+        // TODO 게임 종료시 해야할 로직을 호출한다.
+        // TODO 게임 결과를 반환해야 한다.
+        return new BaseResponse<>(null);
     }
 
 }
