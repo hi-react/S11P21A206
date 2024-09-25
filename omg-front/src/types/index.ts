b2316d6e784aaa8a7b3b8c238f664d33c292cc3f
HannaKang <hanna0175@gmail.com>
2024-09-25 16:35:40 +0900

    [#24] Feat: 게임중 유저 정보 타입 정의 추가
    - 타입 폴더 절대경로 추가 설정

---
+++ omg-front/src/types/index.ts
@@ -0,0 +1,16 @@
+export interface Player {
+  nickname: string;
+  characterType: number;
+  position: number[];
+  direction: number[];
+  hasLoan: number;
+  loanPrincipal: number;
+  loanInterest: number;
+  totalDebt: number;
+  cash: number;
+  stock: number[];
+  goldOwned: number;
+  action: string | null;
+  state: string;
+  isConnected: number;
+}
