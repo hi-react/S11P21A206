8e24be37b51508a8fbf65588926d89828b81920e
Now <khj990322@gmail.com>
2024-09-26 15:34:47 +0900

    [#30] Feat: Add annotations

--- omg-back/src/main/java/com/ssafy/omg/domain/player/entity/PlayerStatus.java
+++ omg-back/src/main/java/com/ssafy/omg/domain/player/entity/PlayerStatus.java
@@ -1,7 +1,7 @@
 package com.ssafy.omg.domain.player.entity;
 
 public enum PlayerStatus {
-    NOT_STARTED,
-    IN_PROGRESS,
-    COMPLETED
+    NOT_STARTED,  // 이번 라운드 거래 내역 없음
+    IN_PROGRESS,  // 거래소에서 거래중
+    COMPLETED     // 이번 라운드 거래 완료
 }
