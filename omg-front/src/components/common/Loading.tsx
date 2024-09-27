607c351b3daebe2ac9196ec73a5da15fd2654de6
HannaKang <hanna0175@gmail.com>
2024-09-27 17:11:31 +0900

    [#32] Fix: 렌더링 코드 수정

--- omg-front/src/components/common/Loading.tsx
+++ omg-front/src/components/common/Loading.tsx
@@ -1,20 +1,14 @@
 import { useContext, useEffect } from 'react';
 
-import { useSocketMessage } from '@/stores/useSocketMessage';
-import useUser from '@/stores/useUser';
 import { SocketContext } from '@/utils/SocketContext';
 import { useProgress } from '@react-three/drei';
 
 export default function Loading(): JSX.Element | null {
-  const { socket, online, rendered_complete } = useContext(SocketContext);
-  const { nickname } = useUser();
+  const { rendered_complete } = useContext(SocketContext);
   const { active } = useProgress();
-  const { roomMessage } = useSocketMessage();
-
-  console.log('룸메세지', roomMessage);
 
   useEffect(() => {
-    if (!active && socket && online && roomMessage.sender !== nickname) {
+    if (!active) {
       rendered_complete();
     }
   }, [active]);
