7c9e885052df18b07da1d70f632e2fe79f2bb30d
HannaKang <hanna0175@gmail.com>
2024-09-30 19:31:05 +0900

    [#42] Feat: 소켓 메시지 연동으로 이벤트 카드 동적 업데이트 기능 추가
    - 소켓 메시지 상태를 사용하여 이벤트 타이틀, 내용 및 값이 동적으로 업데이트되도록 구현
    - 금리 변화에 따른 긍정/부정 여부에 따라 텍스트 색상 변경
    - 기존 이벤트 카드 구조 유지하면서 소켓 메시지로부터 수신된 데이터 반영

--- omg-front/src/components/game/EventCard.tsx
+++ omg-front/src/components/game/EventCard.tsx
@@ -1,13 +1,29 @@
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useGesture } from 'react-use-gesture';
 
+import { useSocketMessage } from '@/stores/useSocketMessage';
 import { animated, useSpring } from '@react-spring/web';
 
 const calcX = (y: number, ly: number) =>
   -(y - ly - window.innerHeight / 2) / 20;
 const calcY = (x: number, lx: number) => (x - lx - window.innerWidth / 2) / 20;
 export default function EventCard() {
-  const domTarget = useRef(null);
+  const { eventMessage } = useSocketMessage();
+  const [eventTitle, setEventTitle] = useState('');
+  const [eventContent, setEventContent] = useState('');
+  const [eventValue, setEventValue] = useState(0);
+  const [isPositive, setIsPositive] = useState(false);
+
+  useEffect(() => {
+    if (!eventMessage.title) return;
+
+    setEventTitle(eventMessage.title);
+    setEventContent(eventMessage.content);
+    setEventValue(eventMessage.value);
+    setIsPositive(eventMessage.value >= 0);
+  }, [eventMessage]);
+
+  const domTarget = useRef<HTMLDivElement | null>(null);
   const [{ x, y, rotateX, rotateY, scale }, api] = useSpring(() => ({
     rotateX: 0,
     rotateY: 0,
@@ -40,7 +56,6 @@ export default function EventCard() {
     },
     { domTarget },
   );
-
   return (
     <div
       className='flex items-center justify-center w-[360px] h-[500px]'
@@ -59,14 +74,14 @@ export default function EventCard() {
       >
         <div className='flex flex-col items-center h-full justify-evenly'>
           <h2 className='basis-1/6 text-omg-30b font-omg-title'>
-            대공황 발생!
+            {eventTitle}
           </h2>
-          <span className='text-blue text-omg-18'>금리 하락 4%</span>
-          <p className='font-omg-body text-omg-20'>
-            월스트리트 대폭락으로 시작되어, 미국을 중심으로 세계적으로 경제
-            침체가 발생했습니다. 이로 인해 실업률 급증과 빈곤이 확산되었으며,
-            금융 시스템 붕괴와 물가 하락으로 인한 어려움이 지속됐습니다.
-          </p>
+          <span
+            className={`${isPositive ? 'text-red' : 'text-blue'} text-omg-18`}
+          >
+            {isPositive ? '금리 상승' : '금리 하락'} {eventValue}%
+          </span>
+          <p className='font-omg-body text-omg-20'>{eventContent}</p>
         </div>
       </animated.div>
     </div>
