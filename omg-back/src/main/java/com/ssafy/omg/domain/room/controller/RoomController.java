13afe8c3480c2cd070e3fc30a0e261391d9b9a64
전정민 <junsk50@naver.com>
2024-10-03 23:33:26 +0900

    Fix: nginx 설정

--- omg-back/src/main/java/com/ssafy/omg/domain/room/controller/RoomController.java
+++ omg-back/src/main/java/com/ssafy/omg/domain/room/controller/RoomController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/rooms")
+@RequestMapping("/rooms")
 @RequiredArgsConstructor
 public class RoomController {
 
