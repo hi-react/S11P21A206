d9966becd8a60fe940122e9921f2c3fa0901cdac
HannaKang <hanna0175@gmail.com>
2024-10-05 03:22:22 +0900

    [#69] Feat: 대출 및 상환 컴포넌트 업데이트
    - LoanTake 컴포넌트에서 플레이어의 초기 자산 정보를 불러와 상태를 설정하도록 수정
    - 대출 한도, 현재 자산 및 갚아야 할 돈을 표시하는 UI 추가
    - 대출 신청 완료 후 메시지 초기화 로직 추가
    - LoanRepay 컴포넌트에서 대출 상환 시 잔액 메시지 처리 추가 및 UI 개선
    - 대출 상환 완료 메시지 표시 및 상태 초기화 로직 추가

--- omg-front/src/components/loan-market/LoanRepay.tsx
+++ omg-front/src/components/loan-market/LoanRepay.tsx
@@ -1,31 +1,63 @@
-// import { useContext, useEffect } from 'react';
-import { useEffect } from 'react';
+import { useContext, useEffect } from 'react';
 
+import { useLoanStore } from '@/stores/useLoanStore';
 import { useSocketMessage } from '@/stores/useSocketMessage';
+import { SocketContext } from '@/utils/SocketContext';
 
-// import { SocketContext } from '@/utils/SocketContext';
 import Button from '../common/Button';
 
 export default function LoanRepay() {
-  // const { sellStock } = useContext(SocketContext);
-  const { sellStockMessage } = useSocketMessage();
+  const { repayLoan } = useContext(SocketContext);
+  const { totalDebt, cash, setLoanData } = useLoanStore();
+  const { repayLoanMessage, setRepayLoanMessage } = useSocketMessage();
 
   useEffect(() => {
-    if (!sellStockMessage.message) return;
+    if (repayLoanMessage.message === null || repayLoanMessage.message === '')
+      return;
 
-    alert(sellStockMessage.message);
-  }, [sellStockMessage]);
+    if (repayLoanMessage.isCompleted) {
+      if (repayLoanMessage.message === '0') {
+        alert('대출금을 모두 상환했습니다!');
+      } else {
+        alert(
+          `대출 상환이 완료되었습니다! 남은 대출액: ${repayLoanMessage.message}`,
+        );
+      }
+    } else {
+      alert(repayLoanMessage.message);
+    }
+    setRepayLoanMessage({ message: '', isCompleted: false });
+  }, [setLoanData, repayLoanMessage]);
 
-  const handleRepaying = () => {
-    // 매도 요청
+  const handleClickRepayLoan = () => {
+    const repayLoanAmount = Number(
+      prompt('상환할 대출 액수를 입력하세요.').trim(),
+    );
+    if (repayLoanAmount === 0) {
+      alert('상환액을 다시 입력해주세요.');
+      return;
+    }
+    repayLoan(repayLoanAmount);
   };
 
   return (
     <div className='flex justify-between w-full h-full'>
       <section className='w-[50%] flex justify-center items-center'>
-        <div className='flex flex-col w-full h-full gap-10 px-20 py-10 bg-skyblue'>
+        <div className='flex flex-col w-full h-full gap-10 px-20 py-10 font-omg-body text-omg-24 bg-skyblue'>
+          <p>
+            현재 갚아야 할 남은 돈:
+            <span className='text-omg-28b'>${totalDebt}</span>
+          </p>
+          <p>
+            나의 자산: <span className='text-omg-28b'>${cash}</span>
+          </p>
           <div className='flex items-center justify-center'>
-            <Button text='상환하기' type='trade' onClick={handleRepaying} />
+            <Button
+              text='상환하기'
+              type='trade'
+              onClick={handleClickRepayLoan}
+              disabled={totalDebt === 0}
+            />
           </div>
         </div>
       </section>
--- omg-front/src/components/loan-market/LoanTake.tsx
+++ omg-front/src/components/loan-market/LoanTake.tsx
@@ -2,36 +2,46 @@ import { useContext, useEffect } from 'react';
 
 import { useLoanStore } from '@/stores/useLoanStore';
 import { useSocketMessage } from '@/stores/useSocketMessage';
+import useUser from '@/stores/useUser';
 import { SocketContext } from '@/utils/SocketContext';
 
 import Button from '../common/Button';
 
 export default function LoanTake() {
-  useContext(SocketContext);
-  const { takeLoan, enterLoan } = useContext(SocketContext);
-
-  const { loanLimit } = useLoanStore();
-  const { loanMessage } = useSocketMessage();
-
-  useEffect(() => {
-    enterLoan();
-  }, []);
+  const { takeLoan } = useContext(SocketContext);
+  const { playerIndex } = useUser();
+  const { players } = useContext(SocketContext);
+  const { loanLimit, cash, totalDebt, setLoanData } = useLoanStore();
+  const { loanMessage, setLoanMessage } = useSocketMessage();
 
   useEffect(() => {
-    console.log('loanMessage:', loanMessage);
+    if (players && players[playerIndex]) {
+      const initialCash = players[playerIndex]?.cash;
+      if (initialCash !== undefined) {
+        setLoanData({
+          totalDebt: totalDebt,
+          cash: initialCash,
+        });
+      } else {
+        console.error('현금 정보 불러올 수 없음');
+      }
+    }
+  }, [playerIndex, setLoanData]);
 
+  useEffect(() => {
     if (!loanMessage.message) return;
 
     if (loanMessage.isCompleted) {
       alert(`대출 신청이 완료되었습니다! 대출액: ${loanMessage.message}`);
-    } else if (!loanMessage.isCompleted) {
+    } else {
       alert(loanMessage.message);
     }
-  }, [loanMessage]);
+    setLoanMessage({ message: '', isCompleted: false });
+  }, [loanMessage, setLoanData]);
 
   const handleClickTakeLoan = () => {
     const loanAmount = Number(prompt('대출할 액수를 입력하세요.').trim());
-    if (loanAmount === 0) {
+    if (loanAmount <= 0) {
       alert('대출할 액수를 다시 입력해주세요.');
       return;
     }
@@ -41,14 +51,20 @@ export default function LoanTake() {
 
   return (
     <div className='flex justify-between w-full h-full'>
-      {/* 대출 신청 영역 */}
       <section className='w-[50%] flex justify-center items-center'>
         <div className='flex flex-col w-full h-full gap-10 px-20 py-10'>
           <p className='font-omg-body text-omg-24'>
-            현재 대출 한도는 <span className='text-omg-28b'>${loanLimit}</span>{' '}
+            나의 자산은 <span className='text-omg-28b'>${cash}</span> 이고,
+          </p>
+          <p className='font-omg-body text-omg-24'>
+            현재 대출 한도는 <span className='text-omg-28b'>${loanLimit}</span>
             입니다.
           </p>
-          {/* 대출 버튼 */}
+          <p className='font-omg-body text-omg-24'>
+            현재 갚아야 할 돈은
+            <span className='text-omg-28b'>${totalDebt}</span>
+            입니다.
+          </p>
           <div className='flex items-center justify-center'>
             <Button
               text='대출하기'
