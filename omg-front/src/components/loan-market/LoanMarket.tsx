f38a71923655055351f8c29aaebfc8c933965e77
HannaKang <hanna0175@gmail.com>
2024-10-06 03:01:09 +0900

    [#70] Feat: 대출 모달 UI 및 기능 구현
    - LoanMarket 컴포넌트에서 대출 신청 및 상환 기능 추가
    - 대출 현황 표시 기능 추가
    - 로직 설명 아이콘 및 버튼 추가
    - 대출 정보 컴포넌트 추가
    - 대출 신청/상환 버튼 컴포넌트 추가
    - 카드 형식 컴포넌트와 시트 형식 컴포넌트 전환 기능 추가

--- omg-front/src/components/loan-market/LoanMarket.tsx
+++ omg-front/src/components/loan-market/LoanMarket.tsx
@@ -1,34 +1,30 @@
 import { useContext, useEffect, useState } from 'react';
+import { FaRegQuestionCircle } from 'react-icons/fa';
+import { GiCardPick } from 'react-icons/gi';
+import { BiSpreadsheet } from 'react-icons/bi';
 
-import LoanMain from '@/components/loan-market/LoanMain';
+import { useLoanStore } from '@/stores/useLoanStore';
 import useModalStore from '@/stores/useModalStore';
 import { LoanMarketView } from '@/types';
 import { SocketContext } from '@/utils/SocketContext';
 
 import BackButton from '../common/BackButton';
-import LoanRepay from './LoanRepay';
-import LoanTake from './LoanTake';
+import LoanActionButton from './LoanActionButton';
+import LoanInfo from './LoanInfo';
+import LoanReport from './LoanReport';
+import LoanSheet from './LoanSheet';
 
 export default function LoanMarket() {
-  const { enterLoan } = useContext(SocketContext);
+  const { enterLoan, takeLoan, repayLoan } = useContext(SocketContext);
+  const { totalDebt } = useLoanStore();
 
   const { modals, closeModal } = useModalStore();
   const [currentView, setCurrentView] = useState<LoanMarketView>('main');
+  const [isReportVisible, setIsReportVisible] = useState(true);
 
   useEffect(() => {
     enterLoan();
   }, []);
-
-  const renderComponent = () => {
-    switch (currentView) {
-      case 'take':
-        return <LoanTake />;
-      case 'repay':
-        return <LoanRepay />;
-      default:
-        return <LoanMain setCurrentView={setCurrentView} />;
-    }
-  };
 
   const handleCloseLoanMarket = (e: React.MouseEvent<HTMLDivElement>) => {
     if (e.target === e.currentTarget && modals.loanMarket) {
@@ -46,24 +42,63 @@ export default function LoanMarket() {
     }
   };
 
+
+  const toggleView = () => {
+    setIsReportVisible((prev) => !prev);
+  };
+
   return (
     <div
       className='fixed top-0 left-0 z-50 flex items-center justify-center w-full h-full bg-opacity-70'
       onClick={handleCloseLoanMarket}
     >
       <div className='modal-container'>
-        {/* 시장 수준 */}
-        <section className='relative flex items-center justify-center w-full h-[14%] px-10 py-10 text-black text-omg-40b'>
+        <section className='relative flex items-center justify-center h-[14%] px-10 py-10 text-black text-omg-40b w-full'>
           <div
             className='absolute flex items-center left-10'
             onClick={handleBackButton}
           >
             <BackButton onClick={handleBackButton} />
           </div>
+          <button
+            className='absolute text-center right-10'
+          >
+            <FaRegQuestionCircle size={20} />
+          </button>
         </section>
 
-        {/* 이 아래는 동적으로 변동: 신청/ 상환 */}
-        <section className='flex w-full h-[86%]'>{renderComponent()}</section>
+        <section className='flex w-full h-full'>
+          <div className='relative w-3/5'>
+            <h2 className='text-center text-omg-28b'>보유 대출 리스트</h2>
+            <div className='absolute right-28 top-10'>
+              <button onClick={toggleView} className="transition-transform duration-300 hover:scale-110">
+                {isReportVisible ? (
+                  <BiSpreadsheet size={28} />
+                ) : (
+                  <GiCardPick size={28} />
+                )}
+              </button>
+            </div>
+            {isReportVisible ? <LoanReport /> : <LoanSheet />}
+          </div>
+          <div className='flex flex-col items-center justify-center w-2/5 h-full gap-20'>
+            <LoanInfo />
+            <div className='flex gap-4'>
+              <LoanActionButton
+                actionType='take'
+                buttonText='대출하기'
+                onAction={takeLoan}
+              />
+              <LoanActionButton
+                actionType='repay'
+                buttonText='상환하기'
+                onAction={repayLoan}
+                disabled={totalDebt === 0}
+              />
+            </div>
+            <div><p className='underline text-omg-14'>*해당 대출은 금리가 높은 상품부터 우선적으로 상환됩니다.</p></div>
+          </div>
+        </section>
       </div>
     </div>
   );
