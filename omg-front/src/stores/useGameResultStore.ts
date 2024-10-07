84ac877cf8f07a9d88329cf463c9d4ee4515f13e
HannaKang <hanna0175@gmail.com>
2024-10-07 15:57:24 +0900

    [#76] Feat: 게임 결과 상태 관리를 위한 스토어 생성

---
+++ omg-front/src/stores/useGameResultStore.ts
@@ -0,0 +1,33 @@
+import { create } from 'zustand';
+
+interface PlayerResult {
+  nickname: string;
+  finalGoldCnt: number;
+  finalStockCnt: number[];
+  finalNetWorth: number;
+  finalDebt: number;
+}
+
+interface GameResultData {
+  finalGoldPrice: number;
+  finalStockPrice: number[];
+  playerResults: PlayerResult[];
+}
+
+export const useGameResultStore = create<{
+  finalGoldPrice: number;
+  finalStockPrice: number[];
+  playerResults: PlayerResult[];
+  setGameResultData: (data: GameResultData) => void;
+}>(set => ({
+  finalGoldPrice: 0,
+  finalStockPrice: [],
+  playerResults: [],
+
+  setGameResultData: data =>
+    set({
+      finalGoldPrice: data.finalGoldPrice,
+      finalStockPrice: data.finalStockPrice,
+      playerResults: data.playerResults,
+    }),
+}));
