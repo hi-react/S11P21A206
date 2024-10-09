1b92a54c1dba0ff960163e29b7fd66331d6a34e1
godeeyeon <godeeyeon@gmail.com>
2024-10-09 23:33:45 +0900

    [#93] Design: StockMain에 차트 색상 수정 및 색상 맞춰 아이템 별로 해당 컬러 배경 적용, trade 타입 버튼 수정(이미지 추가, 호버 시 scale-up)

--- omg-front/src/components/common/Button.tsx
+++ omg-front/src/components/common/Button.tsx
@@ -10,7 +10,7 @@ interface ButtonProps {
 export default function Button({ text, type, onClick, disabled }: ButtonProps) {
   const buttonStyles = {
     mainmap: `text-omg-24b bg-white rounded-10 drop-shadow-xl px-8 py-2`,
-    trade: `text-omg-24 bg-blue text-white rounded-10 px-16 py-2`,
+    trade: `text-omg-24 bg-center pb-2 transition-transform duration-200 ease-in-out hover:scale-105`,
     count: 'text-omg-14 px-3 py-1 bg-white rounded-10 drop-shadow-xl',
   };
 
@@ -19,6 +19,16 @@ export default function Button({ text, type, onClick, disabled }: ButtonProps) {
       className={`${buttonStyles[type]} ${disabled ? 'text-gray-500 opacity-50 cursor-not-allowed' : 'opacity-100'}`}
       onClick={onClick}
       disabled={disabled}
+      style={
+        type === 'trade'
+          ? {
+              backgroundImage: `url('/assets/trade-button.png')`,
+              backgroundSize: 'cover',
+              width: '180px',
+              height: '70px',
+            }
+          : {}
+      }
     >
       {text}
     </button>
