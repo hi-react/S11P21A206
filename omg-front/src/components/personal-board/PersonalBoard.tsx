d0a399654587400fe62fd7ab789ffca126b03959
godeeyeon <godeeyeon@gmail.com>
2024-10-14 08:15:30 +0900

    [#93] Design: MainMap 구성 요소들 size up

--- omg-front/src/components/personal-board/PersonalBoard.tsx
+++ omg-front/src/components/personal-board/PersonalBoard.tsx
@@ -38,15 +38,15 @@ export default function PersonalBoard() {
   const renderRankImage = () => {
     if (rank === 1)
       return (
-        <Rank1 className='absolute -right-[2px] z-20 object-contain w-24 h-auto -top-2 drop-shadow-md' />
+        <Rank1 className='absolute z-20 object-contain w-24 h-auto -translate-x-1/2 top-1 left-1/2 drop-shadow-md' />
       );
     if (rank === 2)
       return (
-        <Rank2 className='absolute -right-[3px] z-20 object-contain w-24 h-auto -top-1 drop-shadow-md' />
+        <Rank2 className='absolute z-20 object-contain w-24 h-auto -translate-x-1/2 top-1 left-1/2 drop-shadow-md' />
       );
     if (rank === 3)
       return (
-        <Rank3 className='absolute -right-[1px] z-20 object-contain w-24 h-auto -top-1 drop-shadow-md' />
+        <Rank3 className='absolute z-20 object-contain w-24 h-auto -translate-x-1/2 top-1 left-1/2 drop-shadow-md' />
       );
     return null;
   };
@@ -61,7 +61,7 @@ export default function PersonalBoard() {
           key={i}
           src='/assets/goldbell.png'
           alt='금괴 이미지'
-          className='absolute w-6 drop-shadow-md'
+          className='absolute w-8 mt-2 ml-6 drop-shadow-md'
           style={{
             zIndex: images.length - i,
             left: `${i * 10 + 24}px`,
@@ -81,27 +81,29 @@ export default function PersonalBoard() {
   const animatedDebt = useCountUp(debtRef, totalDebt);
 
   return (
-    <section className='flex justify-center flex-1 h-[104px] -mb-6 text-black fade-in'>
-      <div className='w-[470px] relative flex h-full py-2 bg-white1 bg-opacity-85 border-t-4 border-x-4 border-white rounded-t-10 overflow-hidden items-center shadow-inner'>
+    <section className='flex justify-center flex-1 h-[140px] -mb-6 text-black fade-in'>
+      <div className='w-[700px] relative flex h-full py-2 bg-white1 bg-opacity-85 border-t-4 border-x-4 border-white rounded-t-10 overflow-hidden items-center shadow-inner'>
         <div className='absolute z-18 -top-2 -right-28'>
           <PersonalBG1 className='w-1/2 h-1/2' />
         </div>
         <div className='absolute bottom-0 -left-3 z-18'>
           <PersonalBG2 className='w-2/5 h-auto' />
         </div>
-        <div className='relative flex flex-col justify-center h-full px-4'>
+
+        <div className='relative flex flex-col justify-center h-full gap-2 px-6'>
           {rank !== null && rank !== 4 && renderRankImage()}
           <div className='flex items-center justify-center'>
             <img
               src={characterImageUrl}
               alt={`${nickname} character`}
-              className='object-contain w-14 h-14 drop-shadow-md'
+              className='object-contain w-16 h-16 drop-shadow-md'
             />
           </div>
-          <span className='p-1 mt-2 text-center bg-white text-omg-10 font-omg-body rounded-5 drop-shadow-md'>
+          <span className='p-1 mt-2 text-center bg-white text-omg-14 font-omg-body rounded-5 drop-shadow-md'>
             {nickname}
           </span>
         </div>
+
         <div className='flex flex-col flex-1 h-full'>
           <div className='flex items-center flex-1 w-full h-1/2'>
             <div className='flex justify-around flex-grow w-full h-full'>
@@ -112,26 +114,26 @@ export default function PersonalBoard() {
                   onMouseEnter={() => setHoveredItem(idx)}
                   onMouseLeave={() => setHoveredItem(null)}
                 >
-                  <div className='relative flex items-end h-10'>
+                  <div className='flex items-center h-10'>
                     <img
                       src={`/assets/${item}.png`}
                       alt={`${item} image`}
-                      className={`w-[16px] transition-opacity duration-300 ${hoveredItem === idx ? 'opacity-0' : 'opacity-100'}`}
+                      className={`w-[24px] transition-opacity duration-300 ${hoveredItem === idx ? 'opacity-0' : 'opacity-100'}`}
                     />
                     <span
-                      className={`absolute -translate-x-1/2 left-1/2 bottom-2 text-black transition-opacity duration-300 font-omg-body text-omg-11 break-keep ${hoveredItem === idx ? 'opacity-100' : 'opacity-0'}`}
+                      className={`absolute -translate-x-1/2 left-1/2 bottom-2 text-black transition-opacity duration-300 font-omg-body text-omg-18 break-keep ${hoveredItem === idx ? 'opacity-100' : 'opacity-0'}`}
                     >
                       {treeItemNameInKorean(item)}
                     </span>
                   </div>
-                  <span className='absolute top-0 text-black right-2 font-omg-body text-omg-14'>
+                  <span className='absolute top-0 text-black right-2 font-omg-body text-omg-18'>
                     {stock[idx + 1]}
                   </span>
                 </div>
               ))}
             </div>
           </div>
-          <div className='flex flex-col px-6 text-omg-11 font-omg-body'>
+          <div className='flex flex-col px-6 text-omg-18 font-omg-body'>
             <div className='flex justify-between w-full'>
               <span>보유 현금</span>
               <span ref={cashRef}>${formatNumberWithCommas(animatedCash)}</span>
@@ -142,11 +144,11 @@ export default function PersonalBoard() {
             </div>
           </div>
         </div>
-        <div className='relative flex flex-col items-center justify-center px-8 h-4/5 text-omg-11 font-omg-body'>
-          <span>보유 금 개수</span>
+        <div className='relative flex flex-col items-center justify-center px-8 py-12 w-1/8 h-4/5 text-omg-18 font-omg-body'>
+          <span className='text-center'>보유 금 개수</span>
           {goldOwned !== 0 && renderGoldImages()}
           <span
-            className={`relative h-full ${goldOwned === 0 ? 'flex items-center justify-center' : 'mt-10'} font-omg-title ${goldOwned === 0 ? 'text-omg-18' : ''}`}
+            className={`relative h-full ${goldOwned === 0 ? 'flex items-center justify-center mt-6' : 'mt-14'} font-omg-title ${goldOwned === 0 ? 'text-omg-20' : ''}`}
           >
             {goldOwned}
             <span> 개</span>
