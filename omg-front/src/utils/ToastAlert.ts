4cba0331ad0d37cadf376952d7b23e0cf648dfe1
dahyun <kdh5005@naver.com>
2024-10-14 21:04:25 +0900

    [#125] Design: Toastalert 텍스트 크기 키움

--- omg-front/src/utils/ToastAlert.ts
+++ omg-front/src/utils/ToastAlert.ts
@@ -5,23 +5,25 @@ export const ToastAlert = (message: string) => {
   Swal.fire({
     toast: true,
     position: 'top',
-    iconHtml: '<img src="/favicon.ico" alt="custom-icon" class="w-8 h-8"/>',
+    iconHtml: '<img src="/favicon.ico" alt="custom-icon" class="w-10 h-10"/>',
     title: message,
     showConfirmButton: false,
-    timer: 3000,
+    timer: 4000,
     timerProgressBar: true,
     customClass: {
       popup:
-        'flex items-center rounded-10 text-white px-4 py-3 text-omg-20 justify-center max-w-3xl',
+        'flex items-center rounded-10 text-white px-3 py-3 text-omg-28 justify-center max-w-3xl',
       timerProgressBar: 'bg-white',
       icon: 'no-icon-style',
+      title: 'flex-grow text-left',
     },
     width: 'auto',
     background:
       'linear-gradient(rgba(1, 30, 118, 0.8), rgba(1, 30, 118, 0.8)),  url("/assets/alert.jpeg") no-repeat center center',
     didOpen: toast => {
       const parentElement = toast.parentNode as HTMLElement;
-      parentElement?.classList.add('pt-28');
+      parentElement?.classList.add('pt-20');
+      toast.style.backgroundSize = 'cover';
       toast.onmouseenter = Swal.stopTimer;
       toast.onmouseleave = Swal.resumeTimer;
     },
